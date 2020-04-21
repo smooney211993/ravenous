1219785fbca538ae1d9706ac92fa17ce6d9b0743
smooney211993 <sm.athome21@gmail.com>
2020-04-21 21:31:04 +1000

    added sortbyoptions object and searchbar class with a function

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,9 @@
 import React from "react";
 import "./SearchBar.css"
+
+const sortByOptions = {
+    "Best Match" : 'best_match',
+    "Highest Rated" : "rating",
+    "Most Reviewed" : "review_count"
+
+}
