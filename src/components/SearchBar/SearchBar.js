ed921ad778391e7e4068330c029feed958415aba
smooney211993 <sm.athome21@gmail.com>
2020-04-22 12:46:27 +1000

    returning an array of list tag

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./SearchBar.css"
 
-const sortByOptions = {
-    "Best Match" : 'best_match',
-    "Highest Rated" : "rating",
-    "Most Reviewed" : "review_count"
+    const sortByOptions = {
+        "Best Match" : 'best_match',
+        "Highest Rated" : "rating",
+        "Most Reviewed" : "review_count"
 
-}
+    }
 
 class SearchBar extends React.Component {
     renderSortByOptions(){
-        
+            return Object.keys(sortByOptions).map((sortByOption)=>{
+                let sortByOptionsValue = sortByOptions[sortByOption]
+                return <li key={sortByOptionsValue}> {sortByOption} </li>
+            })
     }
 }
