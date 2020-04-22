dfce782d8c0823ad65887c086a9e232d7a31a533
smooney211993 <sm.athome21@gmail.com>
2020-04-22 14:04:35 +1000

    finalised first part of project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import BusinessList from './components/BusinessList/BusinessList' ;
 class App extends React.Component {
   render(){
     return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>
       
     );
   }
