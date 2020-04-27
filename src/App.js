5788cff7784f4f09d1c8b533b2011de2786aacfa
smooney211993 <sm.athome21@gmail.com>
2020-04-27 14:15:38 +1000

    created map

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList' ;
+import businessess from './components/Business/businessess.js'
 
 class App extends React.Component {
   render(){
     return (
-      <div class="App">
+      <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businessess={businessess} /> 
       </div>
       
     );
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,20 @@ class BusinessList extends React.Component {
     render (){
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {this.props.businessess.map((user, i)=>{
+                    return <Business 
+                    key = {i}
+                    imageSrc ={this.props.businessess[i].imageSrc}
+                    name = {this.props.businessess[i].name}
+                    address = {this.props.businessess.address} 
+                    city = {this.props.businessess[i].city}
+                    state = {this.props.businessess[i].state}
+                    zipCode = {this.props.businessess[i].zipCode}
+                    category = {this.props.businessess[i].category}
+                    rating = {this.props.businessess[i].rating}
+                    reviewCount = {this.props.businessess[i].reviewCount}/>
+
+                })}
             </div>
         )
 
